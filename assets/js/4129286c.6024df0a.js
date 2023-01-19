c53e2fb0b75aeab4d114116ba81c998257f26aec
René Werner <rene@gofiber.io>
2023-01-19 21:52:04 +0100

    remove docusaurus files from the root -> because everything is now in the /docs folder

--- assets/js/4129286c.6024df0a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfiber_docs=self.webpackChunkfiber_docs||[]).push([[5156],{2776:s=>{s.exports=JSON.parse('{"name":"@easyops-cn/docusaurus-search-local","id":"default"}')}}]);
